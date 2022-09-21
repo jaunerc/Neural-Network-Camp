a29c4486e0141b001024aa494045a0a4d7d6278c
jaunerc <cyrill.jauner@zuehlke.com>
2022-09-21 16:32:20 +0200

    add running version

--- src/main/kotlin/Files.kt
+++ src/main/kotlin/Files.kt
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.rows
 import org.jetbrains.kotlinx.dataframe.io.readCSV
+import java.io.File
+import javax.imageio.ImageIO
 
 fun readCsv(path: String): List<List<Int>> {
     val df = DataFrame.readCSV(fileOrUrl = path, header = MutableList(785) {"A"}) // use a specific header so that the first line is also proceeded
@@ -10,4 +12,9 @@ fun readCsv(path: String): List<List<Int>> {
             .map { v -> v as Int}
             .toList() }
         .toList()
+}
+
+fun readPng(path: String): List<Int> {
+    val img = ImageIO.read(File(path))
+    return listOf()
 }
--- src/main/kotlin/NeuralNetwork.kt
+++ src/main/kotlin/NeuralNetwork.kt
@@ -3,6 +3,7 @@ import org.jetbrains.kotlinx.multik.api.linalg.dot
 import org.jetbrains.kotlinx.multik.api.ndarray
 import org.jetbrains.kotlinx.multik.api.rand
 import org.jetbrains.kotlinx.multik.ndarray.data.D1Array
+import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
 import org.jetbrains.kotlinx.multik.ndarray.operations.minus
 import org.jetbrains.kotlinx.multik.ndarray.operations.plusAssign
 import org.jetbrains.kotlinx.multik.ndarray.operations.times
@@ -11,38 +12,38 @@ class NeuralNetwork(
     inputNodes: Int,
     hiddenNodes: Int,
     outputNodes: Int,
-    private val activation: (D1Array<Double>) -> D1Array<Double>,
+    private val activation: (D2Array<Double>) -> D2Array<Double>,
     private val learningRate: Double
 ) {
     private val weightsInputHidden = Multik.rand<Double>(hiddenNodes, inputNodes) - 0.5
     private val weightsHiddenOutput = Multik.rand<Double>(outputNodes, hiddenNodes) - 0.5
 
     fun train(inputs: List<Double>, targets: List<Double>) {
-        val inputsNd = Multik.ndarray(inputs)
-        val targetsNd = Multik.ndarray(targets)
+        val inputsNd = Multik.ndarray(inputs, 784, 1)
+        val targetsNd = Multik.ndarray(targets, 10, 1)
 
         val hiddenInputs = Multik.linalg.dot(weightsInputHidden, inputsNd)
-        val hiddenOutputs = activation(hiddenInputs)
+        val hiddenOutputs = sigmoid2(hiddenInputs)
 
         val finalInputs = Multik.linalg.dot(weightsHiddenOutput, hiddenOutputs)
-        val finalOutputs = activation(finalInputs)
+        val finalOutputs = sigmoid2(finalInputs)
 
         val outputErrors = targetsNd - finalOutputs
         val hiddenErrors = Multik.linalg.dot(weightsHiddenOutput.transpose(), outputErrors)
 
         weightsHiddenOutput += learningRate * Multik.linalg.dot(
             (outputErrors * finalOutputs * (1.0 - finalOutputs)),
-            hiddenOutputs
+            hiddenOutputs.transpose()
         )
 
         weightsInputHidden += learningRate * Multik.linalg.dot(
             (hiddenErrors * hiddenOutputs * (1.0 - hiddenOutputs)),
-            inputsNd
+            inputsNd.transpose()
         )
     }
 
-    fun query(inputs: List<Double>): D1Array<Double> {
-        val inputsNd = Multik.ndarray(inputs)
+    fun query(inputs: List<Double>): D2Array<Double> {
+        val inputsNd = Multik.ndarray(inputs, 784, 1)
 
         val hiddenInputs = Multik.linalg.dot(weightsInputHidden, inputsNd)
         val hiddenOutputs = activation(hiddenInputs)
