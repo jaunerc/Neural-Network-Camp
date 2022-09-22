23a546c8267bc98150de6ebd67b53d0cf2286ae5
jaunerc <cyrill.jauner@zuehlke.com>
2022-09-22 10:00:54 +0200

    extract functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,55 +1,8 @@
-fun main(args: Array<String>) {
-    val neuralNetwork = NeuralNetwork(784, 200, 10, ::sigmoid, 0.1)
+fun main() {
+    val trainingFilePath = "src/main/resources/mnist_train.csv"
+    val testDataPath = "src/main/resources/mnist_test.csv"
 
-    val trainingData = readCsv("src/main/resources/mnist_train.csv")
+    val neuralNetwork = trainNeuralNetwork(trainingFilePath, 0.1, 5)
 
-    trainNetwork(neuralNetwork, trainingData)
-
-    val testData = readCsv("src/main/resources/mnist_test.csv")
-
-    measureScore(neuralNetwork, testData)
-}
-
-fun trainNetwork(neuralNetwork: NeuralNetwork, trainingData: List<List<Int>>) {
-    for (i in 1..5) {
-        trainingData.stream()
-            .forEach { record -> proceedRecord(neuralNetwork, record) }
-        println("epoch $i done")
-    }
-}
-
-fun proceedRecord(neuralNetwork: NeuralNetwork, record: List<Int>) {
-    val input = record.stream()
-        .skip(1)
-        .map { i -> i.toDouble() / 255.0 * 0.99 + 0.01 }
-        .toList()
-
-    val target = MutableList(10) { 0.01 }
-    target[record[0]] = 0.99
-
-    neuralNetwork.train(input, target)
-}
-
-fun measureScore(neuralNetwork: NeuralNetwork, testData: List<List<Int>>) {
-    val correctAnswers = testData.stream()
-        .map { record -> queryRecord(neuralNetwork, record) }
-        .filter { result -> result == true }
-        .count()
-
-    val score = correctAnswers / testData.size.toDouble()
-
-    println("Correct answers: $score")
-}
-
-fun queryRecord(neuralNetwork: NeuralNetwork, record: List<Int>): Boolean {
-    val input = record.stream()
-        .skip(1)
-        .map { i -> i.toDouble() / 255.0 * 0.99 + 0.01 }
-        .toList()
-
-    val result = neuralNetwork.query(input)
-
-    val indexWithHighestScore = result.data.withIndex().maxByOrNull { it.value }?.index
-
-    return record[0] == indexWithHighestScore
+    measureScore(neuralNetwork, testDataPath)
 }
---
+++ src/main/kotlin/MnistTrainer.kt
@@ -0,0 +1,65 @@
+private const val numInputNodes = 784
+private const val numHiddenNodes = 200
+private const val numOutputNodes = 10
+
+fun trainNeuralNetwork(trainingFilePath: String, learningRate: Double, epochs: Int): NeuralNetwork {
+    val neuralNetwork = NeuralNetwork(numInputNodes, numHiddenNodes, numOutputNodes, ::sigmoid, learningRate)
+    val trainingData = readCsv(trainingFilePath)
+
+    trainNetwork(neuralNetwork, trainingData, epochs)
+
+    return neuralNetwork
+}
+
+fun trainNetwork(neuralNetwork: NeuralNetwork, trainingData: List<List<Int>>, epochs: Int) {
+    for (i in 1..epochs) {
+        trainingData.stream()
+            .forEach { record -> proceedRecord(neuralNetwork, record) }
+        println("epoch $i done")
+    }
+}
+
+private fun proceedRecord(neuralNetwork: NeuralNetwork, record: List<Int>) {
+    val input = record.stream()
+        .skip(1)
+        .map(::normalizeRecord)
+        .toList()
+
+    val target = createTargetRecord(record[0])
+
+    neuralNetwork.train(input, target)
+}
+
+private fun createTargetRecord(correctNumberIndex: Int): MutableList<Double> {
+    // the target record has a high value of almost 1 at the correct number index and all other values are almost 0
+    val target = MutableList(numOutputNodes) { 0.01 }
+    target[correctNumberIndex] = 0.99
+    return target
+}
+
+fun measureScore(neuralNetwork: NeuralNetwork, testDataPath: String) {
+    val testData = readCsv(testDataPath)
+    val correctAnswers = testData.stream()
+        .map { record -> queryRecord(neuralNetwork, record) }
+        .filter { result -> result == true }
+        .count()
+
+    val score = correctAnswers / testData.size.toDouble()
+
+    println("score: $score")
+}
+
+fun queryRecord(neuralNetwork: NeuralNetwork, record: List<Int>): Boolean {
+    val input = record.stream()
+        .skip(1)
+        .map(::normalizeRecord)
+        .toList()
+
+    val result = neuralNetwork.query(input)
+
+    val indexWithHighestScore = result.data.withIndex().maxByOrNull { it.value }?.index
+
+    return record[0] == indexWithHighestScore
+}
+
+private fun normalizeRecord(grayValue: Int) = grayValue.toDouble() / 255.0 * 0.99 + 0.01
